30957f5588f5c52e1282dc5b03339b5c78b81ae2
Rawda577 <rawda.577@gmail.com>
2023-08-11 19:34:13 -0700

    0x01. C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all alphabet letters expect q and e.
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	if (letter != 'q' && letter != 'e')
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
