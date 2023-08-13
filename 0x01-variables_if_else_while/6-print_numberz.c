db72e33853be4803ce9808e0cfc155db58a90a57
Rawda577 <rawda.577@gmail.com>
2023-08-12 17:16:03 -0700

    0x01. C

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -12,13 +12,13 @@ int main(void)
 {
 	int digit = 0;
 
-	while (digit <= 9)
+	while(digit <= 9)
 	{
+		/*convert digit to ASCII representation*/
 		putchar(digit + '0');
 		digit++;
 	}
-
-	printf("\n");
+	putchar('\n');
 
 	return (0);
 }
