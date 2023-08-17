bd70645b4a32cd0fec9f0019ec9af48fcd5e9ff3
Rawda577 <rawda.577@gmail.com>
2023-08-16 17:53:02 -0700

    0x02. C

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,9 +3,8 @@
 /***
  * times_table - prints the 9 times table
  *
- * Example Table
- * 0, 0, 0, 0, ..
- * 0, 1, 2, 3, ..
+ * Prints the multiplication table for numbers 0 to 9.
+ *
 */
 
 void times_table(void)
