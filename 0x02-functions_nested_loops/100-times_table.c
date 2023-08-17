9fbac6a8b9f139babea732ce8a5519ab3462476c
Rawda577 <rawda.577@gmail.com>
2023-08-16 20:20:20 -0700

    0x02. C

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,26 +17,32 @@ void print_times_table(int n)
 			_putchar(48);
 			for (mult = 1; mult <= n; mult++)
 			{
+				_putchar('0');
 				_putchar(',');
 				_putchar(' ');
 
 				prod = num * mult;
 
 				if (prod <= 9)
+				{
 					_putchar(' ');
+					_putchar(' ');
+				}
 				else if (prod <= 99)
+				{
 					_putchar(' ');
+				}
 
-				if (prod >= 100)
+				_putchar((prod / 100) + '0');
+				_putchar(((prod / 10) % 10) + '0');
+				_putchar((prod % 10) + '0');
+
+
+				if (mult < n)
 				{
-					_putchar((prod / 100) + 48);
-					_putchar(((prod / 10) % 10) + 48);
+					_putchar(',');
+					_putchar(' ');
 				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + 48);
-				}
-					_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
