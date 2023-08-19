22ff8dc134e4ea34457cb47e531c9ce3117e02db
Rawda577 <rawda.577@gmail.com>
2023-08-18 18:11:57 -0700

    0x04. C

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diganola - print a diagonal line
+ *
+ * @n: is the number of times the \ character
+ * slould be printed
+*/
+void print_diganola(int n)
+{
+	int postn, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; postn++)
+		{
+			for (postn = 1; space <= postn; space++)
+				_putchar(' ');
+			-putchar(92); /*is equal to '/' char*/
+			_putchar('\n');
+		}
+	}
+}
