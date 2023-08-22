6fad04e1211f53c0d8795443f35fb3b013b09691
Rawda577 <rawda.577@gmail.com>
2023-08-22 16:47:39 -0700

    0x05. C

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**rest to 98 - tales a pointer to an int paramater
+ * and updates tje value it points to 98
+ *
+ * @n: int paramaater
+ * Return: Nothing
+*/
+
+void resrt_to_98(int *n)
+{
+	*n =98;
+}
