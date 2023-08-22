8b2d1d5ace90f18126c0ac8414e2fff4ff121424
Rawda577 <rawda.577@gmail.com>
2023-08-22 16:16:11 -0700

    0x05. C

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of two integers
+ * using two input paramaters
+ *
+ * @a: input paramater 1
+ * @b: inprt paramater 2
+ *
+ * Return: Nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
