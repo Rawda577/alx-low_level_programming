7d46a90defc7b07d56c260dfe92705038fa04ac5
Rawda577 <rawda.577@gmail.com>
2023-08-22 20:05:34 -0700

    0x05. C

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string to be reversed
+ *
+ * Return: Nothing
+*/
+void rev_string(char *s)
+{
+	int length = 0;
+	char temp;
+	int i;
+
+	while (s[length] != '\0')
+	{
+	length++;
+	}
+
+	for (i = 0; i < length / 2; i++)
+	{
+	temp = s[i];
+	s[i] = s[length - i - 1];
+	+s[length - i - 1] = temp;
+	}
+}
+
