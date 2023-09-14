c800a3cbb7098120182b111e2103fc67dd1bd0da
Rawda577 <rawda.577@gmail.com>
2023-09-13 19:55:45 -0700

    0x0F. C

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,13 +9,14 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
 	if (array && size && cmp)
 		while (i < size)
 		{
 			if (cmp(array[i]))
 				return (i);
+			i++;
 		}
 	return (-1);
 }
