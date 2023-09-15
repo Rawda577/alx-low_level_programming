5d73fd9dfb865e82bae0d39a48a49636a258a9db
Rawda577 <rawda.577@gmail.com>
2023-09-15 11:37:04 -0700

    0x0F. C

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 upon success, 98, 99, or 100 upon failure
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+	printf("Error\n");
+	return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
+	return (99);
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
+
