629a927c10bc8aac4e86bce47c45bc62d4fd6413
Rawda577 <rawda.577@gmail.com>
2023-09-23 08:50:29 -0700

    0x12. C

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) print_before_main() {
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+int main(void) {
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return 0;
+}
