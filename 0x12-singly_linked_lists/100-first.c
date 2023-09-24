140ce64668399fc3d36ae41e650f0f2185576383
Rawda577 <rawda.577@gmail.com>
2023-09-24 09:15:47 -0700

    0x12. C

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,12 +7,19 @@ void print_message(void) __attribute__ ((constructor));
  */
 void print_message(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\\n"
+			"I bore my house upon my back!\\n");
 }
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return 0;
+	printf("(A tortoise, having pretty good sense of a hare's nature, "
+		"challenges one to a race.)\n");
+	return (0);
 }
-
