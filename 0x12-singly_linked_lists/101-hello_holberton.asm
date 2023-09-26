437bbc80360fc198a92df8ce5be531968d188c76
Rawda577 <rawda.577@gmail.com>
2023-09-26 05:53:22 -0700

    0x12. C

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+section .data
+	msg db "Hello, Holberton", 0
+
+section .text
+	extern printf
+	global main
+
+main:
+	; Call printf with the address of the hello string as the argument
+	mov rdi, hello
+	call printf
+
+	; Exit the program
+	mov eax, 0
+	ret
