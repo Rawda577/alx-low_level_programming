807d36c807105d25e46db6476d9fafdc69abab82
Rawda577 <rawda.577@gmail.com>
2023-09-28 18:24:55 -0700

    0x14. C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,21 +9,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
 	unsigned int i = 0;
 
 	if (b == NULL)
-	return (0);
-
-	while (b[i] != '\0')
-	{
-	if (b[i] != '0' && b[i] != '1')
 		return (0);
 
-		num = num << 1;
-		num += b[i] - '0';
-		i++;
+	while (*b)
+	{
+		if (b[i] != '0' && *b != '1')
+			return (0);
+
+		i = i * 2 + (*b++ - '0');
 	}
 
-	return (num);
+	return (i);
 }
