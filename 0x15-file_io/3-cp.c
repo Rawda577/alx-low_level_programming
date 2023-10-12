0b1aef63fc26e4ddb30e9081b9e394fbc756e382
Rawda577 <rawda.577@gmail.com>
2023-10-12 15:50:57 -0700

    0x15. C

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,36 +21,50 @@ int main(int argc, char *argv[])
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
+	{
 		dprintf(2, "Usage: %s source_file destination_file\n", argv[0]);
 		exit(97);
+	}
 
 	source_fd = open(argv[1], O_RDONLY);
 	if (source_fd == -1)
+	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
+	}
 
 	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (dest_fd == -1)
+	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		close(source_fd);
 		exit(99);
+	}
 
 	while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
+	{
 		bytes_written = write(dest_fd, buffer, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			close(source_fd);
 			close(dest_fd);
 			exit(99);
+		}
+	}
 	if (bytes_read == -1)
+	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		close(source_fd);
 		close(dest_fd);
 		exit(98);
+	}
 
 	if (close(source_fd) == -1 || close(dest_fd) == -1)
+	{
 		dprintf(2, "Error: Can't close file descriptor\n");
 		exit(100);
+	}
 
 	return (0);
 }
