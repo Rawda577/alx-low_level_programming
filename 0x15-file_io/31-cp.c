c24e80123971b295a54c264caaeb6b551b34c359
Rawda577 <rawda.577@gmail.com>
2023-10-12 22:03:26 -0700

    0x15. C

--- 0x15-file_io/31-cp.c
+++ 0x15-file_io/31-cp.c
@@ -1,70 +1,92 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include "main.h"
 
 #define BUFFER_SIZE 1024
+#define USAGE "Usage: cp file_source file_dest\n"
+#define ERR_NOREAD "Error: Can't read source file %s\n"
+#define ERR_NOWRITE "Error: Can't write to destination file %s\n"
+#define ERR_NOCLOSE "Error: Can't close file descriptor %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
-/**
- * main - Copies the content of a file to another file.
- * @ac: The argument count.
- * @av: The argument vector.
- * Return: 0 on success, appropriate error code on failure.
- */
-int main(int ac, char *av[])
+void display_usage(void)
+{
+	dprintf(STDERR_FILENO, USAGE);
+}
 
-	int source_fd, destination_fd;
+int open_source_file(const char *source_file)
+{
+	int source_fd = open(source_file, O_RDONLY);
+
+	if (source_fd == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_NOREAD, source_file);
+		exit(98);
+	}
+	return (source_fd);
+}
+
+int open_destination_file(const char *destination_file)
+{
+	int dest_fd = open(destination_file, O_WRONLY |
+			O_CREAT | O_TRUNC, PERMISSIONS);
+
+	if (dest_fd == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_NOWRITE, destination_file);
+		exit(99);
+	}
+	return (dest_fd);
+}
+
+void close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd);
+		exit(100);
+	}
+}
+
+void copy_file(int source_fd, int dest_fd)
+{
 	ssize_t bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
 
-	if (ac != 3)
-	{
-		dprintf(2, "Usage: %s source_file destination_file\n", av[0]);
-		exit(97);
-	}
-
-	source_fd = open(av[1], O_RDONLY);
-	if (source_fd == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-
-	destination_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (destination_fd == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		close(source_fd);
-		exit(99);
-	}
-
 	while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(destination_fd, buffer, bytes_read);
+		bytes_written = write(dest_fd, buffer, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
 		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			close(source_fd);
-			close(destination_fd);
+			dprintf(STDERR_FILENO, ERR_NOWRITE, dest_fd);
+			close_file(source_fd);
+			close_file(dest_fd);
 			exit(99);
 		}
 	}
 
 	if (bytes_read == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		close(source_fd);
-		close(destination_fd);
+		dprintf(STDERR_FILENO, ERR_NOREAD, source_fd);
+		close_file(source_fd);
+		close_file(dest_fd);
 		exit(98);
 	}
+}
 
-	if (close(source_fd) == -1 || close(destination_fd) == -1)
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
 	{
-		dprintf(2, "Error: Can't close file descriptor\n");
-		exit(100);
+		display_usage();
+		exit(97);
 	}
-	return (0);
+
+	int source_fd = open_source_file(argv[1]);
+	int dest_fd = open_destination_file(argv[2]);
+
+	copy_file(source_fd, dest_fd);
+
+	close_file(source_fd);
+	close_file(dest_fd);
+
+	return (EXIT_SUCCESS);
 }
--- 0x15-file_io/35-cp.c
+++ 0x15-file_io/35-cp.c
@@ -20,29 +20,45 @@ int main(int argc, char *argv[])
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, USAGE), exit(97);
+	}
 
 	source_fd = open(argv[1], O_RDONLY);
 	if (source_fd == -1)
+	{
 		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+	}
 
 	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PREMISSIONS);
 	if (dest_fd == -1)
+	{
 		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+	}
 
 	while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
+	{
 		bytes_written = write(dest_fd, buffer, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
 			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+		}
+	}
 	if (bytes_read == -1)
+	{
 		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+	}
 
 	source_fd = close(source_fd);
 	dest_fd = close(dest_fd);
 	if (source_fd)
+	{
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, source_fd), exit(100);
+	}
 	if (dest_fd)
+	{
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, source_fd), exit(100);
+	}
 
 	return (EXIT_SUCCESS);
 }
