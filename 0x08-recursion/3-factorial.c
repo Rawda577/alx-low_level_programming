a79d2be432f7b0a8deda352c0fdef0df82e3b3ec
Rawda577 <rawda.577@gmail.com>
2024-01-14 20:35:38 -0800

    0x08. C

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- * factorian - find factorian
- * @n: int
- * Return: int
-*/
-
-int factorian(int n)
+ * factorial - calculates the factorial of a number
+ * @n: input number
+ *
+ * Return: factorial of the number if n >= 0, otherwise -1
+ */
+int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if (n == 0)
-	{
+	else if (n == 0 || n == 1)
 		return (1);
-	}
-	return (n * factorian(n - 1));
+	else
+		return (n * factorial(n - 1));
 }
-
