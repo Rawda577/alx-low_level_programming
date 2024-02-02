fb921d7e2c018d865a2bcc8079b8ec66790a18de
Rawda577 <rawda.577@gmail.com>
2024-02-02 02:16:15 -0800

    0x12.C

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,11 +9,12 @@
  *
  * Return: address of the new element, or NULL if it failed.
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	list_t *temp;
 
+	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 
@@ -27,14 +28,15 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->next = NULL;
 
 	if (*head == NULL)
+	{
 		*head = new_node;
+	}
 	else
 	{
-		list_t *current = *head;
-
-		while (current->next)
-			current = current->next;
-		current->next = new_node;
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new_node;
 	}
 
 	return (new_node);
