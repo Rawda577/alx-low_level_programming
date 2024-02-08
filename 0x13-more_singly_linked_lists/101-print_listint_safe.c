141e9d98fc7e0a5c91ed89ec15400c620bd043e0
Rawda577 <rawda.577@gmail.com>
2024-02-07 17:20:51 -0800

    0x13.C

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,73 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
 /**
- * free_listint_safe - frees a listint_t list
- * @h: pointer to the head of the list
- *
- * Return: the size of the list that was free’d
+ * struct Node - Node of a singly linked list
+ * @data: Integer data stored in the node
+ * @next: Pointer to the next node
  */
-size_t free_listint_safe(listint_t **h)
+typedef struct Node {
+	int data;
+	struct Node *next;
+} Node;
+
+/**
+ * free_listint_safe - Frees a linked list safely
+ * @h: Double pointer to the head of the list
+ *
+ * Return: Number of nodes freed
+ */
+int free_listint_safe(Node **h)
 {
-	listint_t *temp;
-	size_t count;
+	Node *temp = NULL;
+	int count =   0;
 
-	if (h == NULL || *h == NULL)
-	return (0);
+	if (*h == NULL)
+		return (0);
 
-	count = 0;
-	while (*h)
-	{
+	while (*h) {
 		temp = *h;
 		*h = (*h)->next;
 		free(temp);
 		count++;
 	}
+
 	*h = NULL;
 	return (count);
 }
+
+/**
+ * main - Entry point of the program
+ *
+ * Return:   0 on success
+ */
+int main(void)
+{
+	Node *head = NULL;
+	Node *second = NULL;
+	Node *third = NULL;
+
+	head = (Node *)malloc(sizeof(Node));
+	second = (Node *)malloc(sizeof(Node));
+	third = (Node *)malloc(sizeof(Node));
+
+	head->data =   1;
+	head->next = second;
+
+	second->data =   2;
+	second->next = third;
+
+	third->data =   3;
+	third->next = NULL;
+
+	printf("List before freeing:\n");
+	for (Node *temp = head; temp != NULL; temp = temp->next)
+		printf("%d ", temp->data);
+
+	int count = free_listint_safe(&head);
+
+	printf("\n%d nodes were freed.\n", count);
+
+	return (0);
+}
+
