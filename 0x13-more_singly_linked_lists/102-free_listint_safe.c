09d17e4b84d18795bc4fcfaddc01c7569fb82af0
Rawda577 <rawda.577@gmail.com>
2024-02-07 17:24:28 -0800

    0x13.C

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,38 +1,38 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a listint_t linked list.
- * @h: Pointer to the head of the list.
+ * free_listint_safe - Frees a listint_t linked list safely
+ * @h: Pointer to a pointer to the head of the linked list
  *
- * Return: The size of the list that was freed.
+ * Return: The size of the list that was freed
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *temp;
-	size_t nodes_count = 0;
+	size_t count = 0;
+	listint_t *current, *next;
 
 	if (h == NULL || *h == NULL)
-	return (0);
+		return (0);
 
 	current = *h;
-	while (current != NULL)
+	while (current)
 	{
-	nodes_count++;
-	temp = current;
-	current = current->next;
+		count++;
+		next = current->next;
 
-	if (temp < current)
-	{
-		free(temp);
-	}
-	else
+		/* Check if the current node has already been visited */
+		if (((void *)current - (void *)current->next) >= 0)
 		{
-		*h = NULL;
-		return (nodes_count);
+			free(current);
+			*h = NULL;
+			break;
 		}
+
+		free(current);
+		current = next;
 	}
 
 	*h = NULL;
-	return (nodes_count);
+	return (count);
 }
+
